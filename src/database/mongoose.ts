1122ec1c3e7e0bc6e5a71a334673ab4bff3dd22a
Kater Akeren <akerenkater@gmail.com>
2021-06-30 04:39:05 +0100

    update mongoose uri

--- src/database/mongoose.ts
+++ src/database/mongoose.ts
@@ -1,18 +1,14 @@
 import { connect } from 'mongoose';
 
-const dbUri = process.env.MONGODB_URL;
-
 (async (): Promise<void> => {
   try {
-    if (dbUri) {
-      await connect(dbUri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-      });
-      // console.log('DB connection established!');
-    }
+    await connect(process.env.MONGODB_URL as string, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log('DB connection established!');
   } catch (error) {
     console.error(error);
   }
