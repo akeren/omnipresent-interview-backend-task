ffb9f2040db9e031b85e73a53fe2d673135bed31
Kater Akeren <akerenkater@gmail.com>
2021-06-30 04:46:50 +0100

    add morgan

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
+import morgan from 'morgan';
 import express, { Request, Response } from 'express';
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.get('/', (req: Request, res: Response): void => {
   res.json({
